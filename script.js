06d40ebb5ab9f56d303b5f9ac54f5e14cf1bf4ab
deepan25 <deepan251999@gmail.com>
2022-08-11 17:14:18 +0530

    add-filr.

---
+++ script.js
@@ -0,0 +1,182 @@
+// 1 CAR
+
+let car ={
+    brand:"mahenthra",
+    name:"thor",
+    color:"black",
+    fuel:"diasel"
+}
+console.log("my car",car.brand,car.name,car.color,car.fuel);
+console.log(car);
+
+// 2 .PERSON
+let person ={
+    name:"deepan",
+    age:23,
+    phone:9944367507,
+    sibiling:{
+        brotherName:"mathan",
+        age:20,
+    }
+}
+console.log(person);
+console.log(person.sibiling.brotherName);
+
+// 3.PHONE
+let phone ={
+    model:"vivo",
+    color:"blue",
+    price:18000,
+    proseser:"dragon"
+}
+console.log(phone);
+console.log(phone.price);
+
+// 4.LAPTOP
+let laptop ={
+    model:"lenova",
+    color:"black",
+    price:40000
+}
+console.log(laptop);
+console.log(laptop.price);
+
+// 5.bike
+let bike ={
+    model:"royal enfield",
+    color:"balck",
+    fuel:"diasal",
+    cost:150000
+}
+console.log(bike);
+console.log(bike.fuel);
+
+// 6.pencil
+let pencil={
+    model:"absara",
+    color:"yellow",
+    cost:10
+}
+console.log(pencil);
+console.log(pencil.cost);
+
+// 7.headphone
+let headphone ={model:"boat",price:450,color:"white",gurenty:"1 year"}
+console.log(headphone);
+console.log(headphone.gurenty);
+
+// 8.TV
+let tv ={model:"sumsung",price:45000,color:"black",gurenty:"5 year"}
+console.log(tv);
+console.log(tv.gurenty);
+
+// 9.car
+let a={Brand:"Maruthi",Name:"Swift",Color:"Red",FuelType:"Diesel"};
+console.log(a.FuelType);
+console.log(a);
+
+// 10 bike
+let b={Brand:"Hero",Name:"Splender",FuelType:"Petrol",Color:"Blue and black"};
+console.log(b.Color);
+console.log(b);
+
+// 11.door
+let c={Brand:"Teak",Name:"Wood",lockType:"Key",Color:"Maroon"};
+console.log(c.name);
+console.log(c);
+
+// 12.phone
+let e={Brand:"Redmi",Name:"Note 8",Ram:"4GB",Color:"Black"};
+console.log(e.Brand,e.color);
+console.log(e);
+
+// 13.laptop
+let f={Brand:"sumsung",Name:"sumsung E-415-25",Ram:"4GB",Color:"Black"};
+console.log(f.Ram);
+console.log(f);
+
+// 14.laptop
+let g={Brand:"DELL",Name:"Inspiron 5500",Ram:"4GB",Color:"Gunmetal"};
+console.log(g.Color);
+console.log(g);
+
+// 15.bus
+let h={Brand:"Tata",Name:"Bus",FuelType:"Diesel",Color:"Red and Yellow"};
+console.log(h.FuelType,h.Color);
+console.log(h);
+
+// 16.bus
+let i={Brand:"Ashok Leyland",Name:"Bus",FuelType:"Diesel",Color:"Blue and Lightwhite"};
+console.log(i.Brand,i.Name,i.FuelType,i.Color);
+console.log(i);
+
+// 17.car
+let j={Brand:"Tesla",Name:"cybertuck",FuelType:"Electric",Color:"White"};
+console.log(j.Color);
+console.log(i);
+
+// 18 .car
+let k={Brand:"Toyoto",Name:"Fortuner",FuelType:"Petrol",Color:"White"};
+console.log(k.Color);
+console.log(k);
+
+// 19 .car
+let m={Brand:"Mahindra",Name:"Scorpio",FuelType:"Petrol",Color:"Black"};
+console.log(m.Color);
+console.log(m);
+
+// 20.id card
+let id={name:"mathan",dept:"bca",regno:2016222,blood:"AB+"}
+console.log(id.);
+console.log(id.blood);
+
+// 21.cricter
+const o={Name:"Dhoni",best:"finiser",Type:"Cricket"};
+console.log(o.Type);
+console.log(o);
+
+// 22.cricter
+const p={Name:"Mr",Role:"IPL",Type:"Cricket"};
+console.log(p.Role,p.Type);
+console.log(p);
+
+// 23.cricter
+const q={Name:"Boult",Role:"Headset",Type:"wireless"};
+console.log(q.Name,q.Role,q.Type);
+console.log(q);
+
+// 24.mobile
+const r={Name:"Oppo",Role:"Multiple",Type:"Android"};
+console.log(r.Name,r.Type);
+console.log(r);
+
+// 25.mobile
+const s={Name:"Honor 6s",Role:"Multiple",Type:"Android"};
+console.log(s.Type);
+console.log(s);
+
+// 26.cricter
+const t={Name:"Cricket",Role:"Multiple",Type:"All Formats"};
+console.log(" My favourite game is ",t.Name,t.Type);
+console.log(t);
+
+// 27.sim
+const sim={Name:"Reliance",Role:"Give the air",Type:"1200RPM"};
+console.log(,sim.Type);
+console.log(sim);
+
+// 28.fAN
+const v={Name:"Crompton",Role:"Give the brightness",Type:"LED"};
+console.log(v.Type);
+console.log(v);
+
+// 29.BROADBAND
+const w={Name:"Winner",Role:"Wifi ",Type:"Wireless"};
+console.log(,w.Type);
+console.log(w);
+
+// 30.MOBILE
+const x={Name:"IPHONE",Role:"Android",Type:"7A"};
+console.log(x.Type);
+console.log(x);
+
